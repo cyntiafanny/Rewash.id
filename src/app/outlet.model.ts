bedc35ba216a8d8375eed08f751012a8803dced8
cyntiafanny <cyntia.fanny@student.umn.ac.id>
2020-11-15 15:21:37 +0700

    outlet

--- src/app/outlet.model.ts
+++ src/app/outlet.model.ts
@@ -1,3 +1,12 @@
 export interface Outlet {
-
+  id: string,
+  name: string,
+  location: string,
+  latitude: number,
+  longitude: number,
+  openHours: {},
+  admin: {},
+  points: number,
+  rate: number,
+  transactions: number
 }
--- src/app/outlet.service.ts
+++ src/app/outlet.service.ts
@@ -1,13 +1,31 @@
-import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import {Injectable} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {Outlet} from "./outlet.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class OutletService {
-
+  public outlets: Outlet[] = [];
 
   constructor(
-    private firestore: AngularFirestore
-  ) { }
+    private firestore: AngularFirestore,
+  ) {
+  }
+
+  storeOutlet(outlets: Outlet[]) {
+    this.outlets = outlets
+  }
+
+  getAllOutlets() {
+    return [...this.outlets];
+  }
+
+  getOutlet(outletId: string) {
+    return {
+      ...this.outlets.find(outlet => {
+        return outlet.id === outletId;
+      })
+    };
+  }
 }
