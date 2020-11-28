4a59ab0ff1b4f754af09bc6b69d0a0c16ce6a24f
duskysky <duskysky@users.noreply.github.com>
2020-11-28 13:57:05 +0700

    SlidesToLogin

--- src/app/pages/pages.page.ts
+++ src/app/pages/pages.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-pages',
@@ -12,11 +13,15 @@ export class PagesPage implements OnInit {
     speed: 400
   };
 
-  constructor(private splashScreen: SplashScreen) { }
+  constructor(private splashScreen: SplashScreen, private router: Router) { }
 
   ngOnInit() {
       this.splashScreen.show();
 
       this.splashScreen.hide();
+    }
+
+    toLogin(){
+    this.router.navigate(['/authentication']);
     }
 }
