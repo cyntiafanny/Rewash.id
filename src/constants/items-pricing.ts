3d9a91b7f07019173b184d19c2a6d260317d8dc2
duskysky <duskysky@users.noreply.github.com>
2020-11-28 18:48:52 +0700

    pricing

--- src/constants/items-pricing.ts
+++ src/constants/items-pricing.ts
@@ -3,56 +3,56 @@ export const ALL_ITEMS = {
         {
             KEY: 'SHORT_TOPS',
             NAME: 'Short Tops',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 1000,
+            WEIGHT: 140,
             QTY: 0
         },
         {  KEY: 'LONG_TOPS',
             NAME: 'Long Tops',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 1350,
+            WEIGHT: 180,
             QTY: 0
         },
         {
             KEY: 'SHORT_BOTTOMS',
             NAME: 'Short Bottoms',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 1875,
+            WEIGHT: 250,
             QTY: 0
         },
         {
             KEY: 'LONG_BOTTOMS',
             NAME: 'Long Bottoms',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 2600,
+            WEIGHT: 350,
             QTY: 0
         },
         {
             KEY: 'SHORT_OUTWEAR',
             NAME: 'Short Outwear',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 1250,
+            WEIGHT: 180,
             QTY: 0
         },
          {
             KEY: 'LONG_OUTWEAR',
             NAME: 'Long Outwear',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 1875,
+            WEIGHT: 250,
             QTY: 0
         },
          {
             KEY: 'UNDERWEAR',
             NAME: 'Underwear',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 375,
+            WEIGHT: 50,
             QTY: 0
         },
         {
             KEY: 'BRA',
             NAME: 'Bra',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 550,
+            WEIGHT: 70,
             QTY: 0
         },
     ],
@@ -60,99 +60,99 @@ export const ALL_ITEMS = {
         {
             KEY: 'BATIK',
             NAME: 'Batik',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 22000,
+            WEIGHT: 1000,
             QTY: 0
         },
         {
             KEY: 'KEBAYA',
             NAME: 'Kebaya',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 40000,
+            WEIGHT: 1000,
             QTY: 0
         },
         {
             KEY: 'SHORT_DRESS',
             NAME: 'Short Dress',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 22000,
+            WEIGHT: 1000,
             QTY: 0
         },
         {
             KEY: 'LONG_DRESS',
             NAME: 'Long Dres',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 35000,
+            WEIGHT: 1000,
             QTY: 0
         },
         {
             KEY: 'BEDCOVER',
             NAME: 'Bedcover',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 22000,
+            WEIGHT: 1000,
             QTY: 0
         },
         {
             KEY: 'SPREI',
             NAME: 'Sprei',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 18000,
+            WEIGHT: 1000,
             QTY: 0
         },
          {
             KEY: 'PILLOW_COVER',
             NAME: 'Pillow Cover',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 18000,
+            WEIGHT: 1000,
             QTY: 0
         },
         {
             KEY: 'SHOES',
             NAME: 'Shoes',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 25000,
+            WEIGHT: 1000,
             QTY: 0
         },
          {
             KEY: 'MUKENA',
             NAME: 'Mukena',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 22000,
+            WEIGHT: 1000,
             QTY: 0
         },
          {
             KEY: 'SEJADAH',
             NAME: 'Sajadah',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 35000,
+            WEIGHT: 1000,
             QTY: 0
         },
         {
             KEY: 'HIJAB',
             NAME: 'Hijab',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 22000,
+            WEIGHT: 1000,
             QTY: 0
         },
         {
             KEY: 'LARGE_DOLL',
             NAME: 'Large Doll',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 45000,
+            WEIGHT: 1000,
             QTY: 0
         },
         {
             KEY: 'SMALL_DOLL',
             NAME: 'Small Doll',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 20000,
+            WEIGHT: 1000,
             QTY: 0
         },
         {
             KEY: 'SARUNG',
             NAME: 'Saroong',
-            PRICE: 50000,
-            WEIGHT: 200,
+            PRICE: 10000,
+            WEIGHT: 1000,
             QTY: 0
         },
     ]
